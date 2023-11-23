5181b8957b67ba3c52668a9322a0dcf3a00fa777
manoj <mmmanojsharma201611@gmail.com>
2023-11-23 13:33:06 +0530

    Fixed Scroll Issue

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import NavBar from "./Components/NavBar";
 import SideBar from "./Components/SideBar";
 function App() {
   return (
-    <div  style={{ margin: "0px" }}>
-      <div style={{ position: "fixed", top: "0px", left: "0px" , zIndex:"1"}}>
+    <div style={{ margin: "0px" }}>
+      <div style={{ position: "fixed", top: "0px", left: "0px", zIndex: "1" }}>
         <NavBar />
       </div>
       <div style={{ marginTop: "60px", display: "flex" }}>
@@ -23,14 +23,14 @@ function App() {
             position: "fixed",
             top: "60px",
             left: "200px",
-            width: "100%",
+            width: "calc(100vw - 210px)",
             height: "calc(100vh - 60px)",
             overflowY: "auto",
             overflowX: "auto",
             whiteSpace: "nowrap",
           }}
         >
-          <div style={{ width: "2000px" , height:"100%"}}>{Routes}</div>
+          <div style={{ height: "100%" }}>{Routes}</div>
         </div>
       </div>
     </div>
--- src/Common/List.js
+++ src/Common/List.js
@@ -2,21 +2,22 @@ import React from "react";
 function List({ listName, listNotes }) {
   return (
     <div
-      class="card"
-      // style={{
-      //   width: "400px",
-      //   height: "auto",
-      //   overflowX: "hidden",
-      //   border: "1px solid #ccc",
-      // }}
+      style={{
+        border: "1px solid black",
+        width: "300px",
+        maxWidth: "300px",
+        maxHeight: "530px",
+        overflowY: "auto",
+        overflowX: "hidden",
+        marginLeft: "10px",
+        flexShrink: 0,
+        marginRight: "10px",
+      }}
     >
-      <div class="card-body">
+      <div class="card-body" style={{ flexWrap: "wrap" }}>
         <h5 class="card-title">{listName}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-        <div class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </div>
+        <div>Content 1</div>
+
       </div>
     </div>
   );
